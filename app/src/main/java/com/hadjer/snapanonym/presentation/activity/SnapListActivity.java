85332f44497b1ed5f4a641bb30f567ecb98afaa8
hadjer16 <alimazighi_hadjer@hotmail.fr>
2019-03-19 17:05:27 +0100

    end journnée 2

--- app/src/main/java/com/hadjer/snapanonym/presentation/activity/SnapListActivity.java
+++ app/src/main/java/com/hadjer/snapanonym/presentation/activity/SnapListActivity.java
@@ -1,22 +1,50 @@
 package com.hadjer.snapanonym.presentation.activity;
 
+
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.Spinner;
+
 
 import com.hadjer.snapanonym.R;
+import com.hadjer.snapanonym.model.Snap;
+import com.hadjer.snapanonym.presentation.adapter.SnapAdapter;
+import com.hadjer.snapanonym.presentation.loader.SnapLoader;
 
-public class SnapListActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+public class SnapListActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<Snap>> {
+
+    RecyclerView mRecyclerView;
+    List<Snap> mSnaps = new ArrayList<>();
+    SnapAdapter mAdapter;
+    RelativeLayout mSpinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_snap_list);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        mSpinner = findViewById(R.id.spinner);
+        mRecyclerView=findViewById(R.id.recylcerid);
+
+        mAdapter = new SnapAdapter(mSnaps,this);
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        initData();
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -28,4 +56,28 @@ public class SnapListActivity extends AppCompatActivity {
         });
     }
 
+    private void initData(){
+        getSupportLoaderManager().initLoader(0,null,this);
+        mSpinner.setVisibility(View.VISIBLE);
+    }
+    @NonNull
+    @Override
+    public Loader<List<Snap>> onCreateLoader(int i, @Nullable Bundle bundle) {
+
+        return new SnapLoader(this);
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<List<Snap>> loader, List<Snap> snaps) {
+        mSnaps.addAll(snaps);
+        mAdapter.notifyDataSetChanged();
+        mSpinner.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<List<Snap>> loader) {
+
+    }
 }
+
+
